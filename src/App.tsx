7de9efed2f173ea8ddeae4e831e5d503e6ba7c85
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-09-24 02:11:25 +0900

    style: ThemeProvider 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { router } from './Router';
 import GlobalStyle from './styles/GlobalStyle';
+import theme from './styles/theme';
 import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 function App() {
 	return (
 		<>
-			<GlobalStyle />
-			<RouterProvider router={router} />
+			<ThemeProvider theme={theme}>
+				<GlobalStyle />
+				<RouterProvider router={router} />
+			</ThemeProvider>
 		</>
 	);
 }
