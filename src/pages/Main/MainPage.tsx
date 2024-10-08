70cbf22fc249290b355300047bfa644bed9f19b0
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2024-10-08 13:33:03 +0900

    style: MainPage 수정

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,20 +1,5 @@
-import Btn from '@/components/common/Button/Btn';
-import SortBtn from '@/components/common/Button/SortBtn';
-
 function MainPage() {
-	return (
-		<div>
-			<SortBtn />
-			<Btn id="aiClassify" />
-			<Btn id="chooseAll" />
-			<Btn id="cancelClassify" />
-			<Btn id="no" />
-			<Btn id="finishClassify" />
-			<Btn id="ok" />
-			<Btn id="delete" />
-			<Btn id="cancel" />
-		</div>
-	);
+	return <div></div>;
 }
 
 export default MainPage;
