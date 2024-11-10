c6fc30c56f53063d207de2ca52c60f09f5648b81
qkrcodus <130819578+qkrcodus@users.noreply.github.com>
2024-11-11 00:03:09 +0900

    feat : 구글 로그인 구현 완료

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,12 +1,8 @@
 const GOOGLE_URL = import.meta.env.VITE_GOOGLE_URL;
+// const BASE_URL = import.meta.env.VITE_BASE_URL;
 const Login = () => {
 	const handleClick = () => {
-		// 구글 로그인 페이지로 리다이렉트
 		window.location.href = `${GOOGLE_URL}`;
-
-		// 로그인 마치고 백엔드에서 설정한
-		//redirect uri http:/localhost:5173?loggedIN=true 로 이동
-		//라우터 설정한 <MainPage/>컴포넌트 렌더링
 	};
 
 	return (
--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -4,60 +4,29 @@ import { useLocation } from 'react-router-dom';
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 const MainPage = () => {
 	const location = useLocation();
-	// 로그인 마치고 http:/localhost:5173?loggedIN=true로 왔는지 확인
+
 	useEffect(() => {
-		console.log('location.search:', location.search); // 현재 검색 문자열 로그
-
 		const queryParams = new URLSearchParams(location.search);
-		console.log('쿼리 파라미터:', queryParams.toString());
-
 		if (queryParams.get('loggedIn') === 'true') {
-			console.log('로그인 파라미터가 true임, handlePostLogin 호출');
-			handlePostLogin(); // 로그인 후 처리 호출
+			handlePostLogin();
 		} else {
 			console.log('로그인 파라미터가 false 또는 없음');
 		}
 	}, [location]);
 
 	const handlePostLogin = async () => {
-		console.log('handlePostLogin 호출됨');
 		try {
-			// api 추가로 필요한가?
-			const response = await fetch(`${BASE_URL}/login`, {
+			const response = await fetch(`${BASE_URL}/login-check`, {
 				method: 'GET',
-				credentials: 'include', // 쿠키 받아오기
+				credentials: 'include',
 			});
-			if (response.ok) {
-				alert(response);
-			}
-			if (!response.ok) {
-				alert('응답 안옴');
-			}
-
-			// 쿠키에서 refreshToken 가져오기
-			const refreshToken = getCookie('refresh');
-			const accessToken = getCookie('access');
-
-			if (!accessToken || !refreshToken) {
-				console.log('토근 없다');
-			}
-			if (accessToken && refreshToken) {
-				// accessToken을 로컬 스토리지에 저장
-				localStorage.setItem('accessToken', accessToken);
-				localStorage.setItem('refreshToken', refreshToken);
-			}
-
-			console.log('토큰 저장 완료:', { accessToken, refreshToken });
+			const loginData = await response.json();
+			const accessToken = loginData.result.accessToken;
+			localStorage.setItem('access-token', accessToken);
+			console.log('Access token 저장 성공');
 		} catch (error) {
 			console.error('handlePostLogin 중 오류 발생:', error);
 		}
-	};
-
-	// 쿠키에서 특정 값을 가져오는 함수
-	const getCookie = (name: string): string | undefined => {
-		const value = `; ${document.cookie}`;
-		const parts = value.split(`; ${name}=`);
-		if (parts.length === 2) return parts.pop()?.split(';')?.[0];
 	};
 
 	return <div>MainPage</div>;
