6f715726293e63fbb89b69b123c7946cf1f93735
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-12-05 16:46:58 +0900

    feat: 미션페이지 아티클 컴포넌트 구현

--- src/pages/Mission/MissionPage.tsx
+++ src/pages/Mission/MissionPage.tsx
@@ -1,12 +1,23 @@
-import MissionPageComponent from '@/assets/MissionPageComponent.svg?react';
+import DonateIcon from '@/assets/donate.svg?react';
+import DonateArticle from '@/components/Mission/DonateArticle';
+import { DONATE_CONTENT } from '@/constants/\bDonateContents';
 import styled from 'styled-components';
 
 const MissionPage = () => {
 	return (
 		<MissionPageWrapper>
-			<ComponentWrapper>
-				<MissionPageComponent />
-			</ComponentWrapper>
+			<MissionPageTopContainer>
+				<DonateTitleContainer>
+					<DonateIcon />
+					<DonateTitle>DONATE DOTORY</DonateTitle>
+				</DonateTitleContainer>
+
+				<DonateArticleContiner>
+					{DONATE_CONTENT.map(({ id, icon: Icon, title, text }) => (
+						<DonateArticle key={id} icon={Icon} title={title} text={text} />
+					))}
+				</DonateArticleContiner>
+			</MissionPageTopContainer>
 		</MissionPageWrapper>
 	);
 };
@@ -15,14 +26,29 @@ export default MissionPage;
 const MissionPageWrapper = styled.div`
 	width: 100%;
 	background-color: ${({ theme }) => theme.colors.background};
-	position: relative;
-	z-index: 0;
 	display: flex;
-	position: relative;
+	flex-direction: column;
 	min-height: 100vh;
+	padding: 10.1rem 29.8rem;
 `;
-const ComponentWrapper = styled.div`
-	position: absolute;
-	left: 29.8rem;
-	top: 7.1rem;
+
+const MissionPageTopContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 1.7rem;
+`;
+
+const DonateTitleContainer = styled.div`
+	display: flex;
+	gap: 0.8rem;
+`;
+
+const DonateTitle = styled.h1`
+	${({ theme }) => theme.fonts.Pretendard_Semibold_38px};
+	color: ${({ theme }) => theme.colors.white1};
+`;
+
+const DonateArticleContiner = styled.div`
+	display: flex;
+	gap: 3.3rem;
 `;
