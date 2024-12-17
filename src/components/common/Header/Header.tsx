3aca881f331da4ed7c36403eda224af388fdcef6
KIMGEONHWI <dirnrlarjsgn@naver.com>
2024-12-18 03:46:18 +0900

    feat: 도토리 사용, 적립 내역 조회 api 연결

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -4,16 +4,29 @@ import BellIcon from '@/assets/Bell.svg?react';
 import HelpIcon from '@/assets/Help.svg?react';
 import InformationIcon from '@/assets/Information.svg?react';
 import LogoIcon from '@/assets/Logo.svg?react';
+import MiniAcornIcon from '@/assets/MiniAcorn.svg?react';
 import PlusFileIcon from '@/assets/PlusFile.svg?react';
 import { useBookmarkContext } from '@/contexts/BookmarkContext';
 import useModal from '@/hooks/useModal';
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+interface DotoryHistory {
+	id: string;
+	content: string;
+	acorn: number;
+	addedAt: string;
+}
 
 function Header() {
 	const { addBookmark } = useBookmarkContext();
 	const { isOpen: isModalOpen, openModal, closeModal } = useModal();
 	const [url, setUrl] = useState('');
+	const [isBellOpen, setIsBellOpen] = useState(false);
+	const [earn, setEarn] = useState<DotoryHistory[]>([]);
 
 	const handleAddBookmark = async () => {
 		await addBookmark(url);
@@ -21,6 +34,26 @@ function Header() {
 		setUrl('');
 		closeModal();
 	};
+
+	const toggleBellDropdown = () => setIsBellOpen((prev) => !prev);
+
+	useEffect(() => {
+		const getDotory = async () => {
+			try {
+				const accessToken = localStorage.getItem('access-token');
+				const response = await axios.get(`${BASE_URL}/api/v1/acorns-use/history`, {
+					params: { page: 1 },
+					headers: { access: accessToken },
+				});
+
+				setEarn(response.data.result);
+			} catch (error) {
+				console.error('Error fetching missions:', error);
+			}
+		};
+
+		getDotory();
+	}, []);
 
 	return (
 		<HeaderWrapper>
@@ -30,7 +63,33 @@ function Header() {
 				<PlusFileIcon onClick={openModal} style={{ cursor: 'pointer' }} />
 			</HeaderLeftContent>
 			<HeaderRightContent>
-				<BellIcon />
+				<BellIcon onClick={toggleBellDropdown} style={{ cursor: 'pointer' }} />
+				{isBellOpen && (
+					<BellDropdown>
+						<CloseButton onClick={toggleBellDropdown}>x</CloseButton>
+						<DropDownTitleContainer>
+							<MiniAcornIcon />
+							<DropDownTitle>DOTORY HISTORY</DropDownTitle>
+						</DropDownTitleContainer>
+						<TotalDotoryContainer>
+							<TotalDotory>TOTAL DOTORY</TotalDotory>
+							<Total>50</Total>
+						</TotalDotoryContainer>
+
+						<HistoryContainer>
+							{Array.isArray(earn) && earn.length > 0 ? (
+								earn.map((item) => (
+									<HistoryItem key={item.addedAt}>
+										<TotalDotory>{item.content}</TotalDotory>
+										<Total>{item.acorn}</Total>
+									</HistoryItem>
+								))
+							) : (
+								<p></p>
+							)}
+						</HistoryContainer>
+					</BellDropdown>
+				)}
 				<HelpIcon />
 				<InformationIcon />
 			</HeaderRightContent>
@@ -89,4 +148,75 @@ const Input = styled.input`
 	::placeholder {
 		color: #555;
 	}
+`;
+
+const BellDropdown = styled.div`
+	position: absolute;
+	top: 12rem;
+	right: 32.5rem;
+	width: 38.2rem;
+	height: 50.7rem;
+	background-color: ${({ theme }) => theme.colors.background};
+	border-radius: 15px;
+	border: 1.75px solid ${({ theme }) => theme.colors.gray1};
+	box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+	padding: 4.8rem 4.5rem;
+	color: ${({ theme }) => theme.colors.white1};
+	z-index: 1000;
+`;
+
+const DropDownTitleContainer = styled.div`
+	display: flex;
+	gap: 0.517rem;
+	align-items: center;
+`;
+
+const DropDownTitle = styled.h1`
+	display: flex;
+	${({ theme }) => theme.fonts.Pretendard_Semibold_22px};
+`;
+
+const TotalDotoryContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	background: ${({ theme }) => theme.colors.orange2};
+	padding: 1.1rem 1.8rem;
+	border-radius: 10px;
+	border: solid 3px ${({ theme }) => theme.colors.orange1};
+	margin-top: 1.2rem;
+`;
+
+const TotalDotory = styled.p`
+	${({ theme }) => theme.fonts.Pretendard_Semibold_13px};
+`;
+
+const Total = styled.p`
+	color: ${({ theme }) => theme.colors.orange1};
+	${({ theme }) => theme.fonts.Pretendard_Semibold_13px};
+`;
+
+const HistoryContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
+	margin-top: 2.8rem;
+`;
+
+const HistoryItem = styled.div`
+	display: flex;
+	justify-content: space-between;
+	background-color: ${({ theme }) => theme.colors.gray1};
+	border: 3px solid ${({ theme }) => theme.colors.bookmark_hover};
+	border-radius: 10px;
+	padding: 1.1rem 1.8rem;
+`;
+
+const CloseButton = styled.button`
+	position: absolute;
+	top: 1rem;
+	right: 3.5rem;
+	color: ${({ theme }) => theme.colors.white1};
+	font-size: 3rem;
+	cursor: pointer;
 `;
